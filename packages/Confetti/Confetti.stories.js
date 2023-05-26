092cfdfc62a5026b853705cd30d4dd2065b31be9
durulkoca <durulkoca@gmail.com>
2023-05-26 18:52:50 +0300

    migrate smalllogo and confetti

---
+++ packages/Confetti/Confetti.stories.js
@@ -0,0 +1,10 @@
+import Confetti from "./Confetti";
+
+export default {
+  title: "Nucleoid/Confetti",
+  component: Confetti,
+};
+
+export const Default = {
+  args: {},
+};
---
+++ packages/SmallLogo/SmallLogo.stories.js
@@ -0,0 +1,8 @@
+import SmallLogo from "./SmallLogo";
+
+export default {
+  title: "Nucleoid/SmallLogo",
+  component: SmallLogo,
+};
+
+export const Default = {}; // This component doesn't receive any props
