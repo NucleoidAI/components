137765442711d51e836b33001b1c41e72ac439c3
322332 <gokhan.demirtas@msn.com>
2023-05-26 15:08:13 +0300

    initialize components

---
+++ packages/Logo/Logo.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Box, Typography } from "@mui/material";
+import styles from "./styles";
+
+function Logo({ title = "", sx, large }) {
+  return (
+    <Box sx={{ ...sx }}>
+      <Box sx={styles.root}>
+        <Box>
+          <Typography
+            display={"inline"}
+            fontSize={large ? "35px" : "22px"}
+            color={"#209958"}
+            sx={{ pr: 0.5 }}
+          >
+            Nucleoid
+          </Typography>
+
+          <Typography
+            display={"inline"}
+            fontSize={large ? "20px" : "14x"}
+            color={"#dfdfdf"}
+          >
+            {title}
+          </Typography>
+        </Box>
+      </Box>
+    </Box>
+  );
+}
+
+export default Logo;
