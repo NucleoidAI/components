fa52969d5968eb50b49a5dabc98df639d0b1b358
durulkoca <durulkoca@gmail.com>
2023-05-27 20:04:31 +0300

    Add openaicodeexplainbutton story

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,14 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
-  stories: ["../packages/**/*.mdx", "../packages/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: [
+    "../packages/**/*.mdx",
+    "../packages/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "@storybook/addon-actions",
   ],
   framework: {
     name: "@storybook/react-vite",
