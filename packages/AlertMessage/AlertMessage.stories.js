5c5d590a78b646c42f2fc9344147d2bf4525fa9a
durulkoca <durulkoca@gmail.com>
2023-05-29 18:13:28 +0300

    Add alertmessage and minor fix minor spelling

---
+++ packages/AlertMessage/AlertMessage.stories.js
@@ -0,0 +1,21 @@
+// AlertMessage.stories.js
+import React from "react";
+import AlertMessage from "./AlertMessage";
+
+export default {
+  title: "Nucleoid/AlertMessage",
+  component: AlertMessage,
+  argTypes: {
+    message: {
+      control: "text",
+      description: "The message to be shown in the alert",
+      defaultValue: "This is an alert message",
+    },
+  },
+};
+
+export const Default = {
+  args: {
+    message: "This is an alert message",
+  },
+};
