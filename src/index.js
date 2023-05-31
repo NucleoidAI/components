6a5414995649e715a3df098d8a487d4f5f85d5b0
322332 <gokhan.demirtas@msn.com>
2023-05-31 22:16:10 +0300

    Move components in src, add index file

---
+++ src/index.js
@@ -0,0 +1,41 @@
+export { default as AddList } from "./AddList";
+export * from "./AddList";
+
+export { default as AlertMessage } from "./AlertMessage";
+export * from "./AlertMessage";
+
+export { default as ChatWindow } from "./ChatWindow";
+export * from "./ChatWindow";
+
+export { default as Confetti } from "./Confetti";
+export * from "./Confetti";
+
+export { default as CopyClipboard } from "./CopyClipboard";
+export * from "./CopyClipboard";
+
+export { default as DeleteDialog } from "./DeleteDialog";
+export * from "./DeleteDialog";
+
+export { default as DescriptionPopover } from "./DescriptionPopover";
+export * from "./DescriptionPopover";
+
+export { default as EditButton } from "./EditButton";
+export * from "./EditButton";
+
+export { default as Logo } from "./Logo";
+export * from "./Logo";
+
+export { default as Page } from "./Page";
+export * from "./Page";
+
+export { default as SmallLogo } from "./SmallLogo";
+export * from "./SmallLogo";
+
+export { default as SummaryForm } from "./SummaryForm";
+export * from "./SummaryForm";
+
+export { default as TabPanel } from "./TabPanel";
+export * from "./TabPanel";
+
+export { default as TableView } from "./TableView";
+export * from "./TableView";
